11f23017c8f67cedf733e0f07acfb3b03da7bc59
Nyingambe Brian <briannmbe@gmail.com>
2023-10-18 00:03:59 +0300

    add pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,20 @@
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import About from "./pages/About";
+import Signin from "./pages/Signin";
+import Signup from "./pages/Signup";
+import Profile from "./pages/Profile";
+
 export default function App() {
-  return <div className="text-red-500">App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-in" element={<Signin />} />
+        <Route path="/sign-up" element={<Signup />} />
+        <Route path="/profile" element={<Profile />} />
+      </Routes>
+    </div>
+  );
 }
